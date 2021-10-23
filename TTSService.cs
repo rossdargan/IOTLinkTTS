8e1efd5740898b7cbe408e45c0e711e85a05d44a
Ross Dargan <ross.dargan@waterstons.com>
2021-10-23 15:13:30 +0100

    Add project files.

---
+++ TTSService.cs
@@ -0,0 +1,30 @@
+﻿using IOTLinkAPI.Addons;
+using IOTLinkAPI.Platform.Events.MQTT;
+using System.Speech.Synthesis;
+
+namespace IOTLinkAddon
+{
+    public class TTSService : ServiceAddon
+    {
+        public override void Init(IAddonManager addonManager)
+        {
+            base.Init(addonManager);
+
+            GetManager().SubscribeTopic(this, "speak", OnSpeakMessage);
+        }
+
+        private void OnSpeakMessage(object sender, MQTTMessageEventEventArgs e)
+        {
+            string message = e.Message.GetPayload();
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                return;
+            }
+
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                synth.Speak(message);
+            }
+        }
+    }
+}
